07d75d555fe6b4da555fa239513ce3f33f924faf
Joshua Pelealu <johndoe@example.com>
2021-10-20 12:57:02 -0400

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,7 @@
+<script>
+	let dark = false;
+	import { Button } from 'attractions';
+	import { CheckIcon } from 'svelte-feather-icons';
+</script>
+
+<Button filled round><CheckIcon size="20" class="mr" />Click Me</Button>
