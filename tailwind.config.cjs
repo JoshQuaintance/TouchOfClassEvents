4a670e55dbdce2942b6e39c09277039a9a1449da
Joshua Pelealu <johndoe@example.com>
2021-10-22 12:57:40 -0400

    Move custom css into Extends

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,6 @@
 // tailwind.config.cjs
 module.exports = {
+	important: true,
 	mode: 'jit',
 	// you dont need `purge: enabled: production` because you are using jit
 	purge: [
@@ -9,27 +10,28 @@ module.exports = {
 	],
 	darkMode: 'class',
 	theme: {
-		extend: {},
-		fontFamily: {
-			// prettier-ignore
-			'neue': '"Comic Neue", cursive'
-		},
-
-		colors: {
-			primary: {
-				lightest: '#8963BB',
-				light: '#744BAA',
-				DEFAULT: '#4A306D',
-				200: '#4A306D',
-				400: '#3A2555'
+		extend: {
+			fontFamily: {
+				// prettier-ignore
+				'neue': '"Comic Neue", cursive'
 			},
 
-			prussian: {
-				lightest: '#1F5784',
-				light: '#174163',
-				DEFAULT: '#0E273C',
-				200: '#081621',
-				400: '#040B11'
+			colors: {
+				primary: {
+					lightest: '#8963BB',
+					light: '#744BAA',
+					DEFAULT: '#4A306D',
+					200: '#4A306D',
+					400: '#3A2555'
+				},
+
+				prussian: {
+					lightest: '#1F5784',
+					light: '#174163',
+					DEFAULT: '#0E273C',
+					200: '#081621',
+					400: '#040B11'
+				}
 			}
 		}
 	},
