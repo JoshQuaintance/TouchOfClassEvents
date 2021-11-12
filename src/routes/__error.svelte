aa63da0be13828b6e1df383bd9ae75646b4be7c5
JoshuaPelealu <hasianjoshua@gmail.com>
2021-11-11 22:04:06 -0500

    Added error template

---
+++ src/routes/__error.svelte
@@ -0,0 +1,8 @@
+<script>
+    export const prerender = true;
+</script>
+
+<div class="error-page bg-cameo-pink-lightest w-screen h-screen fixed top-0 left-0 z-[100] overflow-hidden">
+    An error has occurred
+    <slot />
+</div>
