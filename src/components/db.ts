5e72b5f862f648d9adf4fb95b8b3e6326b95a1cb
JoshuaPelealu <hasianjoshua@gmail.com>
2021-11-19 21:18:51 -0500

    Redundant Code

--- src/components/db.ts
+++
@@ -1,8 +0,0 @@
-import mongodb from 'mongodb';
-import mongoose from 'mongoose';
-
-export function connectDb(): typeof mongoose {
-    mongoose.connect(import.meta.env.VITE_SECRET_MONGO_URI as string);
-
-    return mongoose;
-}
