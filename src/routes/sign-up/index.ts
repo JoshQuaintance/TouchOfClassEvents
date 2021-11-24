dae7aba005a58342338caef7815fd293180c88bf
JoshuaPelealu <hasianjoshua@gmail.com>
2021-11-24 07:59:14 -0500

    Signup endpoint halfway

--- src/routes/sign-up/index.ts
+++ src/routes/sign-up/index.ts
@@ -3,9 +3,68 @@
  * Description: Endpoint for user signup
  */
 
+import type { ServerRequest } from '@sveltejs/kit/types/hooks';
+import type { DefaultBody } from '@sveltejs/kit/types/endpoint';
+import type bcrypt_T from 'bcrypt';
+import type uuid_T from 'uuid';
+import type crypto_T from 'crypto';
 
-export async function post(req) {
+import { connectToDB } from '$utils/db';
 
+export async function post(req: ServerRequest<Record<string, any>, DefaultBody>) {
+    /**
+     *  We dynamically import these modules here so that
+     * it doesn't leak to the other functions that
+     * are being used client side because it might cause some issues
+     * being used client side
+     */
+    const bcrypt: typeof bcrypt_T = await import('bcrypt');
+    const uuid: typeof uuid_T = await import('uuid');
+    const crypto: typeof crypto_T = await import('crypto')
+
+    const { v4: uuidv4 } = uuid;
+    const { mongoose, schemas } = await connectToDB();
+    const { UserSchema } = await schemas;
+
+    const body = JSON.parse(req.body as string);
+    const { email, nickname, password } = body;
+
+    try {
+        // Hash password using 12 rounds of salt
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        const User = mongoose.models.User || mongoose.model('Users', UserSchema);
+
+        const newUser = new User({
+            uid: uuidv4(),
+            email: email,
+            nickname,
+            password: hashedPassword
+        });
+
+        await newUser.save((err) => {
+            if (err)
+                throw {
+                    status: 500,
+                    message: 'Error saving new user credentials',
+                    err
+                };
+        });
+
+        return {
+            status: 201,
+            message: 'Successfully created a new user',
+            code: 'user-created'
+        };
+    } catch (err) {
+        return {
+            status: err.status || 500,
+            body: {
+                message: err.message || 'Server Error',
+                err: err || null
+            }
+        };
+    }
 }
 
 /**
@@ -21,8 +80,7 @@ export async function post(req) {
  * Note: User exist from email code will always precede
  *      user exist form username code
  */
-export async function checkIfUserExist(email: string, nickname: string): Promise<0 | 1 | 2>  {
-
+export async function checkIfUserExist(email: string, nickname: string): Promise<0 | 1 | 2> {
     // using http again, hitting the auth endpoint
     // specifically to check if user exist
     // endpoint src file located: src/routes/auth/user-exist.ts
