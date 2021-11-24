a3b71a4e5e0fc2dace391facf98dd79ba2c25854
JoshuaPelealu <hasianjoshua@gmail.com>
2021-11-24 07:57:56 -0500

    Error page shows status and message

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -2,12 +2,30 @@
     File Location: src/routes/__error.svelte
     Description: Layout/template for error page
 -->
-
-<script>
-    export const prerender = true;
+<script context="module">
+    export const load = ({ error, status }) => ({
+        props: {
+            error,
+            status
+        }
+    });
 </script>
 
-<div class="error-page bg-cameo-pink-lightest w-screen h-screen fixed top-0 left-0 z-[100] overflow-hidden">
-    An error has occurred
-    <slot />
+<script>
+    import Nav from '$components/Nav.svelte';
+
+    export let error;
+    export let status;
+</script>
+
+<div class="error-page bg-cameo-pink-lightest w-screen h-screen top-0 fixed left-0 z-[100] overflow-hidden">
+    <Nav />
+
+    <!-- <div style="margin-top: {$headerHeight}px;" /> -->
+
+    <div class="error-container w-full h-full flex flex-col justify-center items-center">
+
+        <h1 class="text-indigo-800 md:[5.125rem] xl:text-[5.25rem] font-semibold mb-14 md:mb-16">{status}</h1>
+        <h5 class="mt-6 md:mt-8">{error.message}</h5>
+    </div>
 </div>
