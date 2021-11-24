01d7e2faf9f375c766620a324c0a42861ca7d0dc
JoshuaPelealu <hasianjoshua@gmail.com>
2021-11-24 07:58:51 -0500

    Removed auth0 code

--- src/utils/stores.ts
+++ src/utils/stores.ts
@@ -4,14 +4,14 @@
  *  Note: Stores are used to share data between routes
  */
 
-import { writable, derived } from 'svelte/store';
+import { writable } from 'svelte/store';
 import type { Writable } from 'svelte/store';
 
-import type { Auth0Client } from '@auth0/auth0-spa-js';
 
 export const headerHeight = writable(0);
+export const pageLoaded = writable(false);
 
 export const isAuthenticated = writable(false);
-export const user: Writable<Auth0Client | {}> = writable({});
+export const user = writable({});
 export const popupOpen = writable(false);
 export const error = writable();
