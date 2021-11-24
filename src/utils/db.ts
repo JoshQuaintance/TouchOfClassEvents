708100cee0871390f9c6f259597a3ea163f5758b
JoshuaPelealu <hasianjoshua@gmail.com>
2021-11-24 07:58:35 -0500

    Added email to schema

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -17,7 +17,8 @@ async function initSchemas() {
     const schemas = {
         UserSchema: new mongoose.Schema({
             uid: { type: String, required: true, unique: true },
-            nickname: { type: String, required: true },
+            email: { type: String, required: true, unique: true },
+            nickname: { type: String, required: true, unique: true },
             password: { type: String, required: true }
         })
     };
