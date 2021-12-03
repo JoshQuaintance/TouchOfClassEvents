c7fd8e7a0574441d73f9eadc60c00e30dbd2f4cd
JoshuaPelealu <hasianjoshua@gmail.com>
2021-12-03 12:19:11 -0500

    Try to not store public key

--- src/routes/auth/auth-utils.ts
+++ src/routes/auth/auth-utils.ts
@@ -1,4 +1,3 @@
-
 /**
  * File Location: src/routes/auth/auth-utils.ts
  * Description: Utilities for authentications
@@ -7,6 +6,9 @@ import type { SignOptions, VerifyOptions } from 'jsonwebtoken';
 
 import jwt from 'jsonwebtoken';
 import crypto from 'crypto';
+
+export const PUBLIC_RSA_KEY =
+    'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlJQklqQU5CZ2txaGtpRzl3MEJBUUVGQUFPQ0FROEFNSUlCQ2dLQ0FRRUFxMWNaUHUyWU93TnhGVnhoalN5egpwdjRlc0grODA1TFFYait3blNWdVdEOVRiVStDVlEva3NDblFFWUpzQ2xHRk1JNzMwYkwrellGei9DdU4vd1NZCnQ5bS9LUjVnQW1uOENJaEFINFFqdU1IaDl3Y1EvMzZDanYvdTFtYkdRdFRlS0lWZ0k0dVdkSmR3QUUvUDNKNVYKbHdnaWo3bHBWQzQzei9BWmJlcVhrLzZKVXpPbVJoYW5Qd0NVcEJsUW5hZ3hkSlExakZabHpsQUxJQWl1V1ZITwp0ZUtzdU9BQnpIZlUzbHVuS2NPNHdzZWpzeWlUMzE5bi8vakdwbEJZRDlJWEFNME5HenpKRWNiTE1XdXowTnpVCkxLdUcxTmdrRmtJb1d2Z1BTL3FXRnNDYjV0UVlFMzRDeS9zdzNteTE5SEJEcittUkhtQ09wNzJrckJFWTE3RU4KWndJREFRQUIKLS0tLS1FTkQgUFVCTElDIEtFWS0tLS0tCg';
 
 export async function generateJWT(payload, jwtSignOptions?: SignOptions) {
     try {
@@ -37,11 +39,7 @@ export async function verifyJWT(token, jwtVerifyOptions?: VerifyOptions) {
     };
 
     try {
-        return jwt.verify(
-            token,
-            Buffer.from(import.meta.env['VITE_SECRET_PUBLIC_RSA_KEY'] as string, 'base64'),
-            verifyOptions
-        );
+        return jwt.verify(token, Buffer.from(PUBLIC_RSA_KEY, 'base64'), verifyOptions);
     } catch (err) {
         console.error(err);
     }
@@ -50,7 +48,7 @@ export async function verifyJWT(token, jwtVerifyOptions?: VerifyOptions) {
 export async function encryptData(data) {
     const encryptedBuffer = crypto.publicEncrypt(
         {
-            key: Buffer.from(import.meta.env['VITE_SECRET_PUBLIC_RSA_KEY'] as string, 'base64').toString(),
+            key: PUBLIC_RSA_KEY,
             padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
             // @ts-ignore
             oeapHash: 'sha256'
