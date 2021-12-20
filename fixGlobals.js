f4051cf6901cf517752b1f22a6233c6eee1101b5
Catalactics <hasianjoshua@gmail.com>
2021-12-20 11:21:17 -0500

    Updated

--- fixGlobals.js
+++ fixGlobals.js
@@ -6,49 +6,57 @@
  */
 import fs from 'fs';
 
-fs.readFile('./node_modules/@pixi/settings/dist/cjs/settings.js', (err, data) => {
-    if (err) throw err;
+// fs.readFile('./node_modules/@pixi/settings/dist/cjs/settings.js', (err, data) => {
+//     if (err) throw err;
 
-    if (data.toString().split('\n')[0] == 'if (!self) self = globalThis.self;')
-        return console.log('@pixi/settings already rewritten');
-    fs.writeFile(
-        './node_modules/@pixi/settings/dist/cjs/settings.js',
-        'if (!self) self = globalThis.self;\n' + data,
-        (err, file) => {
-            if (err) throw err;
-            console.log('Rewrite of @pixi/settings successful!');
-        }
-    );
+//     if (data.toString().split('\n')[0] == 'if (!self) self = globalThis.self;')
+//         return console.log('@pixi/settings already rewritten');
+//     fs.writeFile(
+//         './node_modules/@pixi/settings/dist/cjs/settings.js',
+//         'if (!self) self = globalThis.self;\n' + data,
+//         (err, file) => {
+//             if (err) throw err;
+//             console.log('Rewrite of @pixi/settings successful!');
+//         }
+//     );
+// });
+
+fs.readFile('./fixedPixiBuild.js', (err, data) => {
+    if (err) throw err;
+    fs.writeFile('./node_modules/@pixi/settings/dist/cjs/settings.js', data, (err, file) => {
+        if (err) throw err;
+        console.log('Rewrite of @pixi/settings successful!');
+    });
 });
 
-fs.readFile('./node_modules/@pixi/polyfill/dist/cjs/polyfill.js', (err, data) => {
-    if (err) throw err;
+// fs.readFile('./node_modules/@pixi/polyfill/dist/cjs/polyfill.js', (err, data) => {
+//     if (err) throw err;
 
-    if (data.toString().split('\n')[0] == 'let self = globalThis.self;')
-        return console.log('viewport.js already rewritten');
+//     if (data.toString().split('\n')[0] == 'let self = globalThis.self;')
+//         return console.log('viewport.js already rewritten');
 
-    fs.writeFile(
-        './node_modules/pixi-viewport/dist/cjs/viewport.js',
-        'let let self = globalThis.self;\n' + data,
-        (err, file) => {
-            if (err) throw err;
-            console.log('Rewrite of @pixi/polyfill successfully!');
-        }
-    );
-});
+//     fs.writeFile(
+//         './node_modules/pixi-viewport/dist/cjs/viewport.js',
+//         'let let self = globalThis.self;\n' + data,
+//         (err, file) => {
+//             if (err) throw err;
+//             console.log('Rewrite of @pixi/polyfill successfully!');
+//         }
+//     );
+// });
 
-fs.readFile('./node_modules/pixi-viewport/dist/cjs/viewport.js', (err, data) => {
-    if (err) throw err;
+// fs.readFile('./node_modules/pixi-viewport/dist/cjs/viewport.js', (err, data) => {
+//     if (err) throw err;
 
-    if (data.toString().split('\n')[0] == 'let window = globalThis;')
-        return console.log('viewport.js already rewritten');
+//     if (data.toString().split('\n')[0] == 'let window = globalThis;')
+//         return console.log('viewport.js already rewritten');
 
-    fs.writeFile(
-        './node_modules/pixi-viewport/dist/cjs/viewport.js',
-        'let window = globalThis;\n' + data,
-        (err, file) => {
-            if (err) throw err;
-            console.log('Rewrite viewport.js successfully!');
-        }
-    );
-});
+//     fs.writeFile(
+//         './node_modules/pixi-viewport/dist/cjs/viewport.js',
+//         'let window = globalThis;\n' + data,
+//         (err, file) => {
+//             if (err) throw err;
+//             console.log('Rewrite viewport.js successfully!');
+//         }
+//     );
+// });
