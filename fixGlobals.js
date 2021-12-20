346a58e5f39be3b1a47cd38b2165c1dd7a008686
Catalactics <hasianjoshua@gmail.com>
2021-12-20 10:54:24 -0500

    Fix Build Issues

--- fixGlobals.js
+++ fixGlobals.js
@@ -1,3 +1,9 @@
+/**
+ * This file is used to fix some modules that doesn't like SSR much
+ * and we need to give it the references it needed. I have
+ * changed the code in the main package repo, but it hasn't been merged yet
+ * but when it is merged and served, we will not need the first two edits
+ */
 import fs from 'fs';
 
 fs.readFile('./node_modules/@pixi/settings/dist/cjs/settings.js', (err, data) => {
@@ -11,6 +17,22 @@ fs.readFile('./node_modules/@pixi/settings/dist/cjs/settings.js', (err, data) =>
         (err, file) => {
             if (err) throw err;
             console.log('Rewrite of @pixi/settings successful!');
+        }
+    );
+});
+
+fs.readFile('./node_modules/@pixi/polyfill/dist/cjs/polyfill.js', (err, data) => {
+    if (err) throw err;
+
+    if (data.toString().split('\n')[0] == 'let self = globalThis.self;')
+        return console.log('viewport.js already rewritten');
+
+    fs.writeFile(
+        './node_modules/pixi-viewport/dist/cjs/viewport.js',
+        'let let self = globalThis.self;\n' + data,
+        (err, file) => {
+            if (err) throw err;
+            console.log('Rewrite of @pixi/polyfill successfully!');
         }
     );
 });
