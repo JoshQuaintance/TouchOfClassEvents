9dfefb0be3d65ed3173ce05dfeffd106fd8e83aa
JoshuaPelealu <hasianjoshua@gmail.com>
2022-01-11 13:29:32 -0500

    Responsive nav

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -8,9 +8,22 @@
     import { page } from '$app/stores';
     import Icon from '$components/Icon.svelte';
 
+    let navEl: HTMLElement;
+
     $: userIsSignedIn = $isSignedIn;
-    $: console.log(userIsSignedIn, 'userSignedIn')
+    $: console.log(userIsSignedIn, 'userSignedIn');
+
+    function toggleNav() {
+        navEl.classList.toggle('opened');
+    }
+
+    $: outerWidth = 0;
+    $: innerWidth = 0;
+    $: outerHeight = 0;
+    $: innerHeight = 0;
 </script>
+
+<svelte:window bind:innerWidth bind:outerWidth bind:innerHeight bind:outerHeight />
 
 <div class="header-container fixed top-0 z-[101] w-screen" bind:clientHeight={$headerHeight}>
     <div class="max-w-screen-2xl">
@@ -21,82 +34,83 @@
             </a>
             <!-- logo - end -->
 
+            <style>
+                @media (max-width: 1024px) {
+                    .menu-nav {
+                        clip-path: circle(0px at 100% 5%);
+                        transition: clip-path 0.5s ease-in !important;
+                    }
+
+                    .menu-nav.opened {
+                        clip-path: circle(100%) !important;
+                        transition: clip-path 0.5s ease-out !important;
+                    }
+                }
+            </style>
+
             <!-- nav - start -->
-            <nav class="hidden lg:flex gap-12">
+            <nav
+                class="
+                w-full
+                menu-nav
+                lg:flex z-20
+                justify-around
+                transition-all
+                {innerWidth < 1025 &&
+                    'flex fixed bg-cameo-pink-lightest w-screen h-screen top-0 left-0 flex-col justify-start p-10'}
+                "
+                bind:this={navEl}>
+                <Button class="lg:hidden w-fit group flex" on:click={toggleNav}>
+                    <span class="transform-gpu group-hover:rotate-180 transition-all">
+                        <Icon icon="close-thick" />
+                    </span>
+                </Button>
                 <Button
+                    on:click={toggleNav}
                     href="/"
                     noPrefetch
-                    class="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-xl font-semibold transition duration-100 focus:bg-transparent"
-                >
+                    class="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100 focus:bg-transparent">
                     Home
                 </Button>
 
-                <!-- <div> -->
-                <DropdownShell let:toggle>
-                    <Button
-                        on:click={toggle}
-                        class="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100 focus:bg-transparent"
-                    >
-                        Features
-                        <Icon icon="expand_more" class="fill-current" />
-                    </Button>
-                    <Dropdown>
-                        <div class="padded">
-                            <a
-                                href="#"
-                                class="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100 focus:bg-transparent mt-10 mb-7"
-                            >
-                                Make a reservation
-                            </a>
-                            <a
-                                href="#"
-                                class="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100 focus:bg-transparent"
-                            >
-                                Create an Event
-                            </a>
-                        </div>
-                    </Dropdown>
-                </DropdownShell>
-                <!-- </div> -->
-
-                <style>
-                    .dropdown {
-                        /*position: absolute;*/
-                        margin-left: -30%;
-                        /* transform: translate(0 50%) */
-                    }
-
-                    .dropdown .padded {
-                        width: 12rem;
-                        height: 5rem;
-                        text-align: center;
-                        font-size: 1.2em;
-                        padding-top: 0.5em;
-                    }
-                </style>
+                <Button
+                    on:click={toggleNav}
+                    href="#"
+                    noPrefetch
+                    class="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100 focus:bg-transparent">
+                    Make a reservation
+                </Button>
 
                 <Button
+                    on:click={toggleNav}
+                    href="#"
+                    noPrefetch
+                    class="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100 focus:bg-transparent">
+                    Create an Event
+                </Button>
+
+                <Button
+                    on:click={toggleNav}
                     href="/pricing"
                     noPrefetch
-                    class="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-xl font-semibold transition duration-100 focus:bg-transparent"
-                >
+                    class="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100 focus:bg-transparent">
                     Pricing
                 </Button>
 
                 <Button
+                    on:click={toggleNav}
                     href="/about"
                     noPrefetch
-                    class="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-xl font-semibold transition duration-100 focus:bg-transparent"
-                >
+                    class="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100 focus:bg-transparent">
                     About
                 </Button>
 
                 {#if userIsSignedIn}
                     <Button
+                        on:click={toggleNav}
                         href="/dashboard"
                         noPrefetch
-                        class="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-xl font-semibold transition duration-100 focus:bg-transparent"
-                    >
+                        class="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100 focus:bg-transparent">
                         Dashboard
                     </Button>
                 {/if}
@@ -107,31 +121,42 @@
                         outline: none;
                     }
                 </style>
+                <!-- 
+                        login and sign-up - start
+                        won't show if the user is 
+                        signed in or in sign-up page
+                     -->
+                {#if !userIsSignedIn}
+                    <div class="flex flex-col lg:flex-row justify-start gap-2.5 lg:ml-16">
+                        <a
+                            href="log-in"
+                            class="inline-block focus-visible:ring ring-indigo-300 text-gray-500 hover:text-indigo-500 active:text-indigo-600 text-sm md:text-base font-semibold text-center transition duration-100"
+                            ><Button rectangle>Log in</Button>
+                        </a>
+
+                        <a
+                            href="sign-up"
+                            class="inline-block focus-visible:ring ring-indigo-300 text-sm md:text-base font-semibold text-center transition duration-100"
+                            ><Button filled rectangle class="text-white px-[1.4rem]">Sign up</Button>
+                        </a>
+                    </div>
+                {/if}
             </nav>
+            <Button
+                type="button"
+                class="inline-flex items-center lg:hidden bg-gray-200 hover:bg-gray-300 focus-visible:ring ring-indigo-300 text-gray-500 active:text-gray-700 text-sm md:text-base font-semibold rounded-lg gap-2 px-2.5 py-2"
+                on:click={toggleNav}>
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                    <path
+                        fill-rule="evenodd"
+                        d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                        clip-rule="evenodd" />
+                </svg>
+
+                Menu
+            </Button>
+
             <!-- nav - end -->
-
-            <!-- 
-                login and sign-up - start
-                won't show if the user is 
-                signed in or in sign-up page
-             -->
-            {#if !userIsSignedIn}
-                <div
-                    class="hidden lg:flex flex-col sm:flex-row sm:justify-center lg:justify-start gap-2.5 -ml-8"
-                >
-                    <a
-                        href="log-in"
-                        class="inline-block focus-visible:ring ring-indigo-300 text-gray-500 hover:text-indigo-500 active:text-indigo-600 text-sm md:text-base font-semibold text-center transition duration-100"
-                        ><Button rectangle>Log in</Button>
-                    </a>
-
-                    <a
-                        href="sign-up"
-                        class="inline-block focus-visible:ring ring-indigo-300 text-sm md:text-base font-semibold text-center transition duration-100"
-                        ><Button filled rectangle class="text-white px-[1.4rem]">Sign up</Button>
-                    </a>
-                </div>
-            {/if}
 
             <style>
                 .menu {
